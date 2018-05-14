4d3dacf3f2da9c0b81d40887d88887e1c40965e3
karthikraja34 <rajakarthik131@gmail.com>
2018-05-14 21:25:54 +0530

    used -bodyadhtml

--- powershell script/test2.ps1
+++ powershell script/test2.ps1
@@ -3,11 +3,16 @@ $MailArgs = @{
     To         = '<rajakarthik131@gmail.com>'
     Cc         = @("Marcel <hibekibaca@bitwhites.top>", "Marcelt <ram@bitwhites.top>")
     Subject    = 'A subject '
-    Body       = 'Mail message content goes here! ' 
+    Body       = '  <div style="background:green;padding:10px">
+                    <h1 style="color:#fff"> Mail message content goes here! </h1>
+                    </div>
+                    
+                    <h5>Hi ! This message is typed as html.To use normal message remove <strong>-BodyAsHtml</strong> parameter. </h5>
+                 ' 
     Attachment= "D:\custom works\Playground\shell scripts\powershell script\message.txt"
     SmtpServer = 'smtp.stackmail.com'
     Port       = 25
     UseSsl     = $true
     Credential = New-Object pscredential 'sample@blockchainproject.com',$('Kb7e4624a' |ConvertTo-SecureString -AsPlainText -Force)
 }
-Send-MailMessage @MailArgs
+Send-MailMessage @MailArgs -BodyAsHtml
